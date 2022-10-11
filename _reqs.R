73bc8aff91dd4b249c7dc1ba44070d722c32829d
wzbillings <39701545+wzbillings@users.noreply.github.com>
2022-10-11 12:19:06 -0400

    chore: adds a bunch of dependencies to renv.lock

---
+++ _reqs.R
@@ -0,0 +1,14 @@
+### Require packages that are needed for R Markdown / quarto management
+### but are otherwise not formal dependencies that renv sees.
+
+box::use(
+	markdown,
+	mime,
+	yaml,
+	rmarkdown,
+	lintr,
+	devtools,
+	usethis
+)
+
+# END OF FILE
